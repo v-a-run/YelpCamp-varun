36c2ec9dccddb627784bb3b3af0a6a5ed3ceab86
root <Varun>
2020-07-05 16:37:18 +0000

    add mongoDB as url1

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var port = process.env.PORT || 3000;
 
 var url1 = "mongodb+srv://varun:mongov4run@cluster0-5um30.mongodb.net/yelp_camp?retryWrites=true&w=majority";
 
-var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp_v11';
+// var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp_v11';
 mongoose.connect(url1, { useNewUrlParser : true, useUnifiedTopology: true });
 
 app.use(bodyParser.urlencoded({ extended : true}));
